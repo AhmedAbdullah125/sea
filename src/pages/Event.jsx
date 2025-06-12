b2859294687b33a4601afa5338fa50ffcc8647e8
AhmedAbdullah125 <ahmednabeeh98@gmail.com>
2025-06-12 13:46:24 +0300

    u

--- src/pages/Event.jsx
+++ src/pages/Event.jsx
@@ -3,6 +3,8 @@ import Footer from '../components/footer/Footer'
 import Header from '../components/header/Header'
 import EventHeader from '../components/event/EventHeader'
 import AboutPlace from '../components/event/AboutPlace'
+import EventRateForm from '../components/event/EventRateForm'
+import HotelsTable from '../components/table/HotelsTable'
 const Event = () => {
     return (
         <section>
@@ -10,6 +12,8 @@ const Event = () => {
             {/* Start Page Content */}
             <EventHeader />
             <AboutPlace />
+            <EventRateForm />
+            <HotelsTable title="إكتشــف المزيــد." description={`يُقدم الموقع فعاليات الشهر الحالي ليستمتع المسافر بتجارب سياحية مميزة تشمل مهرجانات، جولات ثقافية، وعروض ترفيهية محلية حية.`} />
             {/* End Page Content */}
             <Footer />
         </section>
