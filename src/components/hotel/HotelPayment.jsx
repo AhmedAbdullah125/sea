d86be88fd10859c51d8ff3bf676aa65945d355b7
AhmedAbdullah125 <ahmednabeeh98@gmail.com>
2025-06-17 12:36:04 +0300

    updTE

---
+++ src/components/hotel/HotelPayment.jsx
@@ -0,0 +1,12 @@
+import React , { useState, useEffect } from 'react'
+
+const HotelPayment = () => {
+
+    return (
+        <section>
+ppp
+        </section>
+    )
+}
+
+export default HotelPayment
