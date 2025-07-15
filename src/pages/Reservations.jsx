797c8623405d381bc409162c9f2947408066eba6
AhmedAbdullah125 <ahmednabeeh98@gmail.com>
2025-07-15 15:45:30 +0300

    updatte

--- src/pages/Reservations.jsx
+++ src/pages/Reservations.jsx
@@ -1,8 +1,51 @@
-
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { API_BASE_URL } from "../lib/apiConfig";
+import Loading from "../components/loading/Loading";
 export default function Reservations() {
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+    const [hotels, setHotels] = useState([]);
+    const [transport, setTransport] = useState([]);
+
+    useEffect(() => {
+        setLoading(true);
+        //scroll to the top of page 
+        window.scrollTo(0, 0);
+        const getData = async () => {
+            try {
+                const response = await axios.get(`${API_BASE_URL}/bookings/booking-travel-visa`, { headers: { 'Authorization': `Bearer ${sessionStorage.getItem('token')}` } });
+                const response2 = await axios.get(`${API_BASE_URL}/bookings/transport`, { headers: { 'Authorization': `Bearer ${sessionStorage.getItem('token')}` } });
+                const response3 = await axios.get(`${API_BASE_URL}/bookings/hotel`, { headers: { 'Authorization': `Bearer ${sessionStorage.getItem('token')}` } });
+                setData(response.data.data);
+                setTransport(response2.data.data);
+                setHotels(response3.data.data);
+                setLoading(false);
+            } catch (error) {
+                console.error('Error retrieving data:', error);
+                setLoading(false);
+                throw new Error('Could not get data');
+            }
+        };
+        getData();
+    }, []);
+    console.log(hotels);
+    console.log(data);
+    console.log(transport);
+
+
+
     return (
-       <div className="p">
-        Reservations
-       </div>
+        <div className="reservation-cont">
+            {
+                loading ? <Loading /> :
+                    data.length == 0 && transport.length == 0 && hotels.length == 0 ? <div className="no-reservation">
+                        <h2>لا يوجد حجوزات سابقة</h2>
+                        <p>يمكنك متابعة كل حجوزاتك من هنا</p>
+                    </div> :
+                        <span>ss</span>
+
+            }
+        </div>
     )
 }
