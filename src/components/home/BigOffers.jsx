96b048a010d25dad6677693e6b31bad96cf61f45
AhmedAbdullah125 <ahmednabeeh98@gmail.com>
2025-07-31 14:14:23 +0300

    update

--- src/components/home/BigOffers.jsx
+++ src/components/home/BigOffers.jsx
@@ -2,10 +2,23 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 import { API_BASE_URL } from '../../lib/apiConfig';
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { motion } from 'framer-motion'
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 const BigOffers = ({ mainData }) => {
     const [data, setData] = useState([])
     const [loading, setLoading] = useState(true);
+    function formatDate(dateString) {
+        const date = new Date(dateString);
+        const day = String(date.getDate()).padStart(2, '0');
+        const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed
+        const year = date.getFullYear();
 
+        return ` ${day}  /  ${month}  /  ${year}`;
+    }
     useEffect(() => {
         setLoading(true);
         const getData = async () => {
@@ -28,6 +41,63 @@ const BigOffers = ({ mainData }) => {
             <div className="container">
                 <h2>عــروض رهيـبة ومميــزات كثيــرة</h2>
                 <p>سواء كنت تحلم بالسفر إلى أوروبا، آسيا، إفريقيا، أستراليا أو أمريكا، نوفر لك خيارات إقامة متنوعة بأسعار تناسب ميزانيتك.</p>
+                <Swiper
+                    pagination={{ clickable: true }}
+                    spaceBetween={20}
+                    // navigation={{ nextEl: `#swiper-btn-next1a`, prevEl: `#swiper-btn-prev1a`, }}
+                    slidesPerView={4}
+                    autoplay={true}
+                    loop={true}
+                    modules={[Autoplay, Navigation, Pagination]}
+                    breakpoints={{
+                        1400: {
+                            slidesPerView: 4,
+                        },
+                        1024: {
+                            slidesPerView: 3,
+                        },
+                        768: {
+                            slidesPerView: 2,
+                        },
+                        640: {
+                            slidesPerView: 1,
+                        },
+                        320: {
+                            slidesPerView: 1,
+                        },
+                        100: {
+                            slidesPerView: 1,
+                        }
+
+                    }}
+                >
+
+                    {
+                        data.map((item, index) => (
+                            <SwiperSlide key={index}>
+                                <motion.div
+                                    initial={{ opacity: 0, y: -30 }}
+                                    whileInView={{ opacity: 1, y: 0 }}
+                                    viewport={{ once: true }}
+                                    transition={{ duration: 0.5 }}
+                                    className="offer">
+                                    <figure className="offer-img">
+                                        <img src={item.image} alt="img" />
+                                    </figure>
+                                    <div className="offer-info">
+                                        <h3>{item.title}</h3>
+                                        <p>{item.description}</p>
+                                        <span className='valid-date'>العــرض ســاري الــى : {formatDate(item.expire_date)} </span>
+                                        <div className="place">
+                                            <span>{item.city_name}, {item.country_name}</span>
+                                        </div>
+                                    </div>
+                                </motion.div>
+                            </SwiperSlide>
+                        ))
+                    }
+
+                </Swiper>
             </div>
         </section>
     )
